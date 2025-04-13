8e1bd5f7686b8192d9ff208b3a26b97ecd9c5224
huxvix <umutergut@hotmail.com>
2025-04-13 16:06:10 +0300

    set up first few pages

---
+++ app/onboarding.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text className='text-2xl'>Onboarding</Text>
+    </View>
+  )
+}
+
+export default Onboarding
+
+const styles = StyleSheet.create({})
